fa096d9e8622492f68d67fdfc8ddf76984a5ee72
Blen <azblen1@gmail.com>
2018-11-27 11:04:41 -0500

    collaborators

---
+++ src/main/java/com/example/demo/CollaboratorsRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CollaboratorsRepository extends CrudRepository<Collaborators, Long> {
+    Collaborators findByLogin(String login);
+}
